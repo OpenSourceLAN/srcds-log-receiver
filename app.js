77491d53db1f146056fb8c060ac5ce7ec0fef746
Chris Holman <squid@sirsquidness.com>
2016-06-05 17:25:08 +1000

    Convert tabs to spaces because spacing

--- app.js
+++ app.js
@@ -13,93 +13,93 @@ log on
 */
 
 var fs = require('fs'),
-	dgram = require('dgram'),
-	moment = require('moment'),
-	buffer = require('buffer');
+    dgram = require('dgram'),
+    moment = require('moment'),
+    buffer = require('buffer');
 
 var logPath = "./logs/",
-	magicBytePassword = 0x53,
-	magicByteNoPassword = 0x52,
-	magicByteEndHeader = 0x4c,
-	packetHeader = new Buffer([255,255,255,255]);
+    magicBytePassword = 0x53,
+    magicByteNoPassword = 0x52,
+    magicByteEndHeader = 0x4c,
+    packetHeader = new Buffer([255,255,255,255]);
 
 var password = new Buffer("12345");
 
 class log {
-	constructor(source) {
-		this.source = source;
-	}
+    constructor(source) {
+        this.source = source;
+    }
 }
 
 // Make sure our log destination exists
 fs.stat(logPath, (err, stat) => {
-	if (err) {
-		fs.mkdir(logPath, (err) => {
-			if (err) {
-				console.error("Couldn't create directory for logs and it doesn't exist already", err);
-			}
-		});
-	}
+    if (err) {
+        fs.mkdir(logPath, (err) => {
+            if (err) {
+                console.error("Couldn't create directory for logs and it doesn't exist already", err);
+            }
+        });
+    }
 });
 
 var socket = dgram.createSocket('udp4');
 
 
 socket.on('message', (msg, rinfo) => {
-	
-	if (msg.slice(0,4).compare(packetHeader) !== 0) {
-		return console.error("Bad packet received");
-	}
-	msg = msg.slice(packetHeader.length);
+    
+    if (msg.slice(0,4).compare(packetHeader) !== 0) {
+        return console.error("Bad packet received");
+    }
+    msg = msg.slice(packetHeader.length);
 
-	if ([/*magicByteNoPassword,*/magicBytePassword].indexOf(msg[0]) == -1) {
-		return console.error("Received wrong packet type");
-	}
-	msg = msg.slice(1);
+    if ([/*magicByteNoPassword,*/magicBytePassword].indexOf(msg[0]) == -1) {
+        return console.error("Received wrong packet type");
+    }
+    msg = msg.slice(1);
 
-	if (checkPassword(msg, password, magicByteEndHeader) == false) {
-		return console.error("Bad password");
-	}
-	msg = msg.slice(password.length+1);
-	
-	// Remove leading space 
-	if (msg[0] == 0x20) {
-		msg = msg.slice(1);
-	}
+    if (checkPassword(msg, password, magicByteEndHeader) == false) {
+        return console.error("Bad password");
+    }
+    msg = msg.slice(password.length+1);
+    
+    // Remove leading space 
+    if (msg[0] == 0x20) {
+        msg = msg.slice(1);
+    }
 
-	// Removes trailing null byte
-	if (msg[msg.length-1] == 0x00) {
-		msg = msg.slice(0,-1);
-	}
-	
-	// Last char is a new line, which we want to log but don't want to display
-	console.log(msg.slice(0,-1).toString());
+    // Removes trailing null byte
+    if (msg[msg.length-1] == 0x00) {
+        msg = msg.slice(0,-1);
+    }
+    
+    // Last char is a new line, which we want to log but don't want to display
+    console.log(msg.slice(0,-1).toString());
 
-	fs.appendFile(logPath + getFilename(rinfo), msg, (err) => {if (err) console.log(err)});
+    fs.appendFile(logPath + getFilename(rinfo), msg, (err) => {if (err) console.log(err)});
 });
 
 socket.on('listening', () => {
-	console.log("Listening!");
+    console.log("Listening!");
 });
 
 socket.bind(9871);
 
 function checkPassword(message, expect, trailByte) {
-	if (message.length < expect.length + 1) {
-		return false;
-	}
-	if (message.slice(0, expect.length).compare(expect) !== 0) {
-		return false;
-	}
-	if (message[expect.length] != trailByte) {
-		return false;
-	}
-	return true;
+    if (message.length < expect.length + 1) {
+        return false;
+    }
+    if (message.slice(0, expect.length).compare(expect) !== 0) {
+        return false;
+    }
+    if (message[expect.length] != trailByte) {
+        return false;
+    }
+    return true;
 }
 
 function getFilename(rinfo) {
-	var date = moment().format("YYYY-MM-DD");
-	return `${date}-${rinfo.address}-${rinfo.port}.log`;
+    var date = moment().format("YYYY-MM-DD");
+    return `${date}-${rinfo.address}-${rinfo.port}.log`;
 }
 
 
